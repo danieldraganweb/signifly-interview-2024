a60f9116c2afbdef161e9b8c20cd1a19bc061405
Daniel Dragan <110283149+danieldraganweb@users.noreply.github.com>
2024-04-09 13:15:17 +0200

    Change layout of the Navbar (#3)

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import styles from "./leaderboard.module.scss";
 import useTeams from "../hooks/useTeams";
-import { TeamFields } from "../types";
 import Image from "next/image";
 
 export default function Leaderboard() {
